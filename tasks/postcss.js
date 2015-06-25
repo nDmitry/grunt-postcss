30a362b84a275b52c6d5cbdf0086c516b32bd85a
Dmitry Nikitenko <dima.nikitenko@gmail.com>
2015-06-25 19:01:43 +0600

    Relative map path for PostCSS. Close #32

--- tasks/postcss.js
+++ tasks/postcss.js
@@ -45,7 +45,7 @@ module.exports = function(grunt) {
         }
 
         if (typeof options.map.annotation === 'string') {
-            annotation = getSourcemapPath(to);
+            annotation = path.relative(path.dirname(to), getSourcemapPath(to));
         }
 
         return annotation;
